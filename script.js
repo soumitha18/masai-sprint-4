51aad2258fa9c7ddc9ecf7806bd91d2e98d23d45
Soumitha Bhaskara <soumithaalpineti@gmail.com>
2020-06-17 17:03:57 +0530

    Modified the subscription page

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 window.addEventListener("load", function(){
     //alert("soumitha")
-    var car  = {
-
-    }
 
     var home = document.getElementById("home")
     home.addEventListener("click", gotoHome)
@@ -17,6 +14,37 @@ window.addEventListener("load", function(){
     sign.addEventListener("click", gotoSign)
 })
 
+var carSubscription  = [
+    ["Tigo (45K kms)", "" , "₹12,999/mo"],
+    ["Swift MT (45K kms)", "" , "₹13,499/mo"],
+    ["Freestyle (45K kms)", "" , "₹13,499/mo"],
+    ["Tata Tiago" , "" , "₹13,499/mo"],
+    ["Swift AT (45K kms)" , "" , "₹14,999/mo"],
+    ["Baleno (45k kms)" ,"", "₹15,999/mo"],
+    ["Volkswagen Polo (Petrol)", "" , "₹16,499/mo"],
+    ["120 (45K kms)", "" ,"₹16,499/mo"],
+    ["Maruti Swift" , "" , "₹17,499/mo"],
+    ["Ford FreeStyle" , "" , "₹17,499/mo"],
+    ["Brezza", "" , "₹18,499/mo"],
+    ["Maruti Swift AT" , "" , "₹18,499/mo"],
+    ["Nexon", "" , "₹18,999/mo"],
+    ["Baleno" , "" , "₹19,499/mo"],
+    ["Tata Nexon", "" , "₹19,499/mo"],
+    ["Nexon AT", "" , "₹20,499/mo"],
+    ["Hyundai i20 Elite" , "" , "₹20,499/mo"],
+    ["Nexon AT (45K kms)" , "" , "₹20,499/mo"],
+    ["Hyundai Creta", "" , "₹22,499/mo"],
+    ["Creta (45K kms)" , "" , "₹22,499/mo"],
+    ["Verna" , "" , "₹23,499/mo"],
+    ["Kicks (Petrol)", "" , "₹23,499/mo"],
+    ["Hexa" , "" , "₹26,499/mo"],
+    ["Mahindra XUV", "" , "₹27,499/mo"],
+    ["XUV 500(45K kms)" , "" , "₹27,499/mo"],
+    ["Mahindra Scorpio", "" , "₹28,999/mo"],     
+    ["Hyundai Creta AT" , "" , "₹32,499/mo"],
+    ["Mahindra XUV AT" , "" , "₹35,499/mo"]
+]
+
 function gotoHome(){
     event.preventDefault()
     location.href = "index.html"
@@ -28,6 +56,10 @@ function gotoDetail(){
 }
 
 function gotoSubscription(){
+    var data = JSON.stringify(carSubscription)
+
+    localStorage.setItem("carSubscription" , data)
+
     event.preventDefault()
     location.href = "subscription.html"
 }
--- subscription.js
+++ subscription.js
@@ -1,3 +1,19 @@
 window.addEventListener("load", function(){
-    
+    var get = document.getElementById("getCars")
+    get.addEventListener("click", function(){
+        var data = localStorage.getItem("carSubscription")
+        var cars = JSON.parse(data)
+        create(cars)
+        //console.log(cars)
+    })
 })
+
+function create(cars){
+    var div = document.getElementById("cars")
+
+    var remove = document.getElementById("position")
+
+    remove.remove()
+
+
+}
